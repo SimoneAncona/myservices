2c6c9f156612d3df814689dd03932084f894f01c
SimoneAncona <simone.ancona00@gmail.com>
2026-02-08 20:23:52 +0100

    Updated build

--- scripts/build-linux-amd64.sh
+++ scripts/build-linux-amd64.sh
@@ -1,2 +1,7 @@
 echo "Building for Linux - AMD64"
-./build-client.sh
+./scripts/build-client.sh
+cd client
+npx electron-forge package --platform=linux --arch=x64
+cd ..
+mkdir ./out 2> /dev/null
+mv ./client/out/mynotes-linux-x64 ./out/linux-amd64
--- scripts/build-win32-amd64.sh
+++ scripts/build-win32-amd64.sh
@@ -1,13 +1,7 @@
 echo "Building for Win32 - AMD64"
-# ./scripts/build-client.sh
-# cd client
-# npx electron-forge package --platform=win32 --arch=x64
-# cd ..
-# mkdir ./out 2> /dev/null
-# mv ./client/out/mynotes-win32-x64 ./out/win32-amd64
-# mv ./out/win32-amd64/mynotes.exe ./out/win32-amd64/ui.exe
-vs=$("C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -property installationPath)
-echo "Found VS at $vs"
-cwd=$(pwd)
-cd "$vs"
-echo "cl" | "$vs/Common7/Tools/VsDevCmd.bat"
+./scripts/build-client.sh
+cd client
+npx electron-forge package --platform=win32 --arch=x64
+cd ..
+mkdir ./out 2> /dev/null
+mv ./client/out/mynotes-win32-x64 ./out/win32-amd64
